11d386b19b391581ea2dc81d7c58695918c40aec
WilsonZhu <winniecmz.zhu@gmail.com>
2022-09-19 17:55:49 -0400

    fix the issue on the transferFileToRecord function

--- Assignment1/A1_Solution/main.cpp
+++ Assignment1/A1_Solution/main.cpp
@@ -17,25 +17,33 @@ int fieldTwoEndIdx(string str) {
     return -1;
 }
 
-vector<Record> transferFileToRecord() {
-    string fileRoute = "../assets/A1_data.txt";
+vector<Record> transferFileToRecord(string theRoute) {
     ifstream readFILE;
-    readFILE.open(fileRoute);
+    readFILE.open(theRoute);
 
     if (!readFILE.is_open()) {
         cout << "Error on file reading!" << endl;
         exit(EXIT_FAILURE);
     }
 
-    vector<Record> allRecords;
+    vector<Record> allRecords; // create a vector of records
+    string textInput;
 
-    char word[50];
-    readFILE >> word;
-    cout << word << endl;
+    while (getline(readFILE, textInput)) {
+        // Output the text from the file
+        allRecords.push_back(allRecords());
+        Record &currentRecord = allRecords.back();
+        currentRecord.field1 = textInput.substr(0, 9);
+        currentRecord.endIndexField2 = fieldTwoEndIdx(textInput);
+        currentRecord.field2 = textInput.substr(10, currentRecord.endIndexField2 - 10);
+        currentRecord.field3 = textInput.substr(currentRecord.endIndexField2 + 1);
+    }
 }
 
 int main() {
-    transferFileToRecord();
+    string FileRoute = "../assets/A1_data.txt";
+    transferFileToRecord(str
+    FileRoute);
 
 
 //    char word[50];
