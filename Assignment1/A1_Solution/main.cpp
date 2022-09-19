5a89e95e55be77d82b4866a5408b130170393654
WilsonZhu <winniecmz.zhu@gmail.com>
2022-09-19 17:36:00 -0400

    enable the txt file importing

--- Assignment1/A1_Solution/main.cpp
+++ Assignment1/A1_Solution/main.cpp
@@ -5,22 +5,47 @@
 
 using namespace std;
 
-int main() {
 
-    string fileRoute = "./assets/A1_data.txt";
+int fieldTwoEndIdx(string str) {
+    int count = 3;
+    for (int idx = 0; idx < str.size(); idx++) {
+        if (isspace(str[idx])) {
+            count -= 1;
+            if (count == 0) return idx;
+        }
+    }
+    return -1;
+}
+
+vector<Record> transferFileToRecord() {
+    string fileRoute = "../assets/A1_data.txt";
     ifstream readFILE;
     readFILE.open(fileRoute);
 
     if (!readFILE.is_open()) {
+        cout << "Error on file reading!" << endl;
         exit(EXIT_FAILURE);
     }
 
+    vector<Record> allRecords;
+
     char word[50];
-    readFILE>>word;
-    while (readFILE.good()){
-        cout<<word<<" ";
-        readFILE>>word;
-    }
+    readFILE >> word;
+    cout << word << endl;
+}
+
+int main() {
+    transferFileToRecord();
+
+
+//    char word[50];
+//    readFILE >> word;
+//    while (readFILE.good()) {
+//        cout << word << " ";
+//        readFILE >> word;
+//    }
+
+    cout << "time space" << endl;
 
 
 //    Record record1("a", "b", "c");
