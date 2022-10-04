7fb2a7e74a84611c53f0e47bf78a71e5072298b4
WilsonZhu <winniecmz.zhu@gmail.com>
2022-10-04 18:18:51 -0400

    fullfill the BP Tree Node key and value aquisation

--- Assignment1/A1_Solution/util/forBPTreeBuild.h
+++ Assignment1/A1_Solution/util/forBPTreeBuild.h
@@ -7,12 +7,39 @@
 
 #endif //A1_SOLUTION_FORBPTREEBUILD_H
 
-# include "iostream"
+#include "iostream"
 #include "../DB_lib/dbComponents.h"
 #include "../newBPTree_lib/newBPTree.h"
-//#include "veryTrickyMethods.h"
 
 using namespace std;
+
+int fieldTwoEndIdx_BPbackup(string aString) {
+    short count = 3;
+    for (short idx = 0; idx < aString.size(); idx++) {
+        if (isspace(aString[idx])) {
+            count -= 1;
+            if (count == 0) return idx;
+        }
+    }
+    return -1;
+}
+
+vector<string> splitTheRecordContent_BPbackup(string input) {
+
+    int aEndIndexField2 = fieldTwoEndIdx_BPbackup(input);
+
+    vector<string> result;
+
+    string aField1 = input.substr(0, 10);
+    string aField2 = input.substr(10, aEndIndexField2 - 11);
+    string aField3 = input.substr(aEndIndexField2 + 1);
+
+    result.push_back(aField1);
+    result.push_back(aField2);
+    result.push_back(aField3);
+
+    return result;
+}
 
 void buildDataFileIntoBPTree(string fileSaveRoute, vector<BlockNode> allBlock) {
 
@@ -24,10 +51,10 @@ void buildDataFileIntoBPTree(string fileSaveRoute, vector<BlockNode> allBlock) {
              relativeIdxOfRecord < allBlock[idxOfBlock].numsOfRecords(); relativeIdxOfRecord++) {
             string rawRecordStringValue = readACertainRecordInBlock(fileSaveRoute, idxOfBlock, relativeIdxOfRecord);
 
-            cout << rawRecordStringValue << endl;
-            cout << endl;
-
-//            vector<string> recordSplitResult = splitTheRecordContent(rawRecordStringValue);
+            // get the split record value
+            vector<string> recordSplitResult = splitTheRecordContent_BPbackup(rawRecordStringValue);
+            string aFieldOne = recordSplitResult[0];
+            string aFieldTwoAndThree = recordSplitResult[1] + recordSplitResult[2];
         }
     }
 }
