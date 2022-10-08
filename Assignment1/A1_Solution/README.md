66bb5cd099f0108a684a08892b477544dacd59b6
WilsonZhu <winniecmz.zhu@gmail.com>
2022-10-07 23:06:38 -0400

    finish on the è basic hash table readme.md

--- Assignment1/A1_Solution/README.md
+++ Assignment1/A1_Solution/README.md
@@ -6,16 +6,45 @@
 
 ****
 
-## <div style="text-align: center;">Part 1</div>
+## <div style="text-align: center;">Part 1 File Storage and B+ Tree</div>
 
 ### A. General Purpose
 
-This script in the Question 1 folder is going to convert the initial data file <b style="color:olive">(named as the 
+This script in the Question 1 folder is going to convert the initial data file <b style="color:lightblue">(named as the
 A1_data.txt)</b> into the block and record for hard drive disk storage
 
-## <div style="text-align: center;">Part 2</div>
+## <div style="text-align: center;">Part 2 - Linear Hashtable</div>
 
-Due to the system and time limitation, the hash table may not be able to explicitly implemented. The following content
-is going to describe the fundamental blueprint of the hash-table build-up strategy.
+Due to the system and time constrain, the hash table may not be able to implemented explicitly. The following content
+is going to generally describe the fundamental blueprint of the hash-table build-up strategy.
 
-1. 
+1. Convert the initial data file (with 46882 records) to several data blocks, and each data block should have the
+   maximum size of 1024 bytes <b style="color:red">(This step is same to the Question1)</b>
+2. When storing each record into the block, the function should return the string-format field 1 value (as the key for
+   the following hashing), as well as two short-format value, one is the index of block where this record stays, the
+   other is the relative index of this record inside the block.
+3. We pre-defined the threshold between the total record (r) and the numbers of bucket (n) to be 5.5. So there would be
+   5.5 records in a single bucket on average.
+4. After finish the block-record storage. A hash function should take each field 1 value from the newly generated data
+   file (which follows the ascending indexing of block and record) as the argument for the hash-value generation. In
+   this case, we will employ the ```std::hash```
+   from [C++ standard library](https://en.cppreference.com/w/cpp/utility/hash) to convert the field 1 into an 8 byte
+   long-format value. For example:
+   ```
+   long value=std::hash<long>()((long)"aaaaaaaaa");
+   cout<<value<<endl;
+   
+   output: 94601286873468
+   ```
+
+5. For each hashed-value from the field 1, we will use a large prime number N to MODE it (only take the residual) into a
+   relatively small value. This primitive number N will be regarded as <b style="color:red"> the total number of buckets
+   for the linear hashing.</b> (with the bucket index from 0 to N-1).
+6. Recall that designed threshold is 5.5, so the final numbers of buckets should not make the r/n exceed 5.5. Therefore,
+   the primative number we are going to choose should be over ```46882 / 5.5 = 8524```
+7. Take the previous 93922831331708 as an example, we use the primitive number 8527, and get the result of
+   ```cout<<94601286873468 % 7901<<endl;``` as 9. So the if the key is "aaaaaaaaa", it will be pointed out into the
+   bucket #9, and there would be 8527 buckets in total.
+8. The real threshold in this case will be around 5.498 ```(46882 / 8527)```
+9. However, the explicit evaluation of hashing performance and hashing collision requires a later programming
+   assessment.
--- Assignment1/A1_Solution/main.cpp
+++ Assignment1/A1_Solution/main.cpp
@@ -1,102 +1,103 @@
-#include <iostream>
-#include <fstream>
-#include <cstdlib>
-#include <algorithm>  // implement sorting
-#include <typeinfo>
+#include <algorithm> // implement sorting
 #include <bitset>
 #include <cmath>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <typeinfo>
 
-#include "util/forBlockANDRecordGeneration.h"
-#include "util/forDataFileCRUD.h"
 #include "DB_lib/dbComponents.h"
 #include "util/forBPTreeBuild.h"
+#include "util/forBlockANDRecordGeneration.h"
+#include "util/forDataFileCRUD.h"
 
 using namespace std;
 
-
 int main() {
 
-//    ofstream myfile;
-//    myfile.open("example.txt");
-//    myfile << "Writing this to a file.\n" << endl;
-//    myfile.close();
-//
-//    fstream newFile;
-//    newFile.open("example.txt");
-//    char value;
-//    newFile.seekg(0, ios::beg);
-//    newFile.get(value);
-//
-//    cout << value << endl;
+  //    ofstream myfile;
+  //    myfile.open("example.txt");
+  //    myfile << "Writing this to a file.\n" << endl;
+  //    myfile.close();
+  //
+  //    fstream newFile;
+  //    newFile.open("example.txt");
+  //    char value;
+  //    newFile.seekg(0, ios::beg);
+  //    newFile.get(value);
+  //
+  //    cout << value << endl;
 
+  // config the raw input data location_asValue
+  string FileRoute = "../assets/A1_data.txt";
+  vector<Record> metaRecord = transferFileToRecord(FileRoute);
 
-    // config the raw input data location_asValue
-    string FileRoute = "../assets/A1_data.txt";
-    vector<Record> metaRecord = transferFileToRecord(FileRoute);
+  // sorting all records base on their field 1
+  sort(metaRecord.begin(), metaRecord.end(),
+       [](const Record &lhs, const Record &rhs) {
+         return lhs.field1 < rhs.field1;
+       });
 
-    // sorting all records base on their field 1
-    sort(metaRecord.begin(),
-         metaRecord.end(),
-         [](const Record &lhs, const Record &rhs) {
-             return lhs.field1 < rhs.field1;
-         });
+  // get how many records in total that need to be store into blocks
+  int totalNumOfRecords = metaRecord.size();
 
-    // get how many records in total that need to be store into blocks
-    int totalNumOfRecords = metaRecord.size();
+  // get the total size of all data in bytes
+  int sizeOfAllRecord = getSizeOfAllData(totalNumOfRecords, metaRecord);
 
-    //get the total size of all data in bytes
-    int sizeOfAllRecord = getSizeOfAllData(totalNumOfRecords, metaRecord);
+  cout << "This script creates " << totalNumOfRecords << " records." << endl;
+  cout << "The total size of all records are " << sizeOfAllRecord << " bytes."
+       << endl;
 
-    cout << "This script creates " << totalNumOfRecords << " records." << endl;
-    cout << "The total size of all records are " << sizeOfAllRecord << " bytes." << endl;
+  vector<BlockNode> metaBlock =
+      storeRecordToBlocks(totalNumOfRecords, metaRecord);
+  int realAllBlockSize = calculateRealBlockSize(metaBlock);
 
-    vector<BlockNode> metaBlock = storeRecordToBlocks(totalNumOfRecords, metaRecord);
-    int realAllBlockSize = calculateRealBlockSize(metaBlock);
+  // get how many blocks we created
+  int totalNumOfBlock = metaBlock.size();
 
-    // get how many blocks we created
-    int totalNumOfBlock = metaBlock.size();
+  // get the total size of all data in bytes
+  int sizeOfAllBlock = totalNumOfBlock * 1024;
 
-    // get the total size of all data in bytes
-    int sizeOfAllBlock = totalNumOfBlock * 1024;
+  cout << "This script creates " << totalNumOfBlock << " blocks." << endl;
+  cout << "The total size of all blocks are " << sizeOfAllBlock << " bytes."
+       << endl;
 
-    cout << "This script creates " << totalNumOfBlock << " blocks." << endl;
-    cout << "The total size of all blocks are " << sizeOfAllBlock << " bytes." << endl;
+  cout << "The space usage rate is "
+       << ((realAllBlockSize + 0.0) / sizeOfAllBlock) * 100 << "%" << endl;
 
-    cout << "The space usage rate is " << ((realAllBlockSize + 0.0) / sizeOfAllBlock) * 100 << "%" << endl;
+  // now we are going to store all blocks (with records) into the datafile
+  string saveRoute = "../assets/dataFile.txt";
 
-    // now we are going to store all blocks (with records) into the datafile
-    string saveRoute = "../assets/dataFile.txt";
+  //    cout << "17 has the total size " << metaBlock[17].currentSize() << endl;
+  //    cout << "17 has the head size " << metaBlock[17].sizeOfHead() << endl;
+  //    cout << "17 has the content size " << metaBlock[17].recordContent.size()
+  //    << endl; writeAllFile(saveRoute, metaBlock);
 
-//    cout << "17 has the total size " << metaBlock[17].currentSize() << endl;
-//    cout << "17 has the head size " << metaBlock[17].sizeOfHead() << endl;
-//    cout << "17 has the content size " << metaBlock[17].recordContent.size() << endl;
-    writeAllFile(saveRoute, metaBlock);
+  //    int quest_blockIdx = 0;
+  //    int quest_relativeRecordIdx = 2;
+  //    cout <<
+  //    metaBlock[quest_blockIdx].endPostionOfEachRecord[quest_relativeRecordIdx
+  //    - 1] << endl; cout <<
+  //    metaBlock[quest_blockIdx].endPostionOfEachRecord[quest_relativeRecordIdx]
+  //    << endl; string astring = readACertainRecordInBlock(saveRoute,
+  //    quest_blockIdx, quest_relativeRecordIdx); cout << astring << endl; cout
+  //    << endl;
 
-//    int quest_blockIdx = 0;
-//    int quest_relativeRecordIdx = 2;
-//    cout << metaBlock[quest_blockIdx].endPostionOfEachRecord[quest_relativeRecordIdx - 1] << endl;
-//    cout << metaBlock[quest_blockIdx].endPostionOfEachRecord[quest_relativeRecordIdx] << endl;
-//    string astring = readACertainRecordInBlock(saveRoute, quest_blockIdx, quest_relativeRecordIdx);
-//    cout << astring << endl;
-//    cout << endl;
+  ssBPTree allNodes;
+  //    buildDataFileIntoBPTree(saveRoute, metaBlock, allNodes);
 
+  //    int global_int = 40;
+  //
+  //    for (int idx = 0; idx < global_int; idx++) {
+  //        string superString = "s" + to_string(idx) + "s";
+  //        allNodes.ramInsertElm(superString, idx);
 
-    ssBPTree allNodes;
-//    buildDataFileIntoBPTree(saveRoute, metaBlock, allNodes);
+  //    }
+  //
+  //    for (int idx = 0; idx < global_int; idx++) {
+  //        string superString = "s" + to_string(idx) + "s";
+  //        allNodes.searchElm(superString);
+  //    }
 
-
-//    int global_int = 40;
-//
-//    for (int idx = 0; idx < global_int; idx++) {
-//        string superString = "s" + to_string(idx) + "s";
-//        allNodes.ramInsertElm(superString, idx);
-
-//    }
-//
-//    for (int idx = 0; idx < global_int; idx++) {
-//        string superString = "s" + to_string(idx) + "s";
-//        allNodes.searchElm(superString);
-//    }
-
-    return 0;
+  return 0;
 }
